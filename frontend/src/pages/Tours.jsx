30e9f64e19386e296f2515a7eb1d0dcd7aae694f
Trung Nguyễn Đức <23521678@ms.uit.edu.vn>
2025-05-29 22:41:22 +0700

    feat: connect Tours.jsx to backend API with pagination

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import CommonSection from "../shared/CommonSection";
 import "../styles/tour.css";
-import tourData from "../assets/data/tours";
+
 import TourCard from "../shared/TourCard";
 import SearchBar from "../shared/SearchBar";
 import Newsletter from "../shared/Newsletter";
@@ -14,10 +14,18 @@ const Tours = () => {
   const [pageCount, setPageCount] = useState(0);
   const [page, setPage] = useState(0);
 
+  const {
+    data: tours,
+    loading,
+    error,
+  } = useFetch(`${BASE_URL}/tours?page=${page}`);
+  const { data: tourCount } = useFetch(`${BASE_URL}/tours/search/getTourCount`);
+
   useEffect(() => {
-    const pages = Math.ceil(5 / 4); //later we will use backend data count
+    const pages = Math.ceil(tourCount / 8); //later we will use backend data count
     setPageCount(pages);
-  }, [page]);
+    window.scrollTo(0, 0);
+  }, [page, tourCount, tours]);
 
   return (
     <>
@@ -31,28 +39,35 @@ const Tours = () => {
       </section>
       <section className="pt-0">
         <Container>
-          <Row>
-            {tourData?.map((tour) => (
-              <Col lg="3" className="mb-4" key={tour.id}>
-                {" "}
-                <TourCard tour={tour}></TourCard>
-              </Col>
-            ))}
+          {loading && (
+            <h4 className="text-center pt-5">Loading...............</h4>
+          )}
+          {error && <h4 className="text-center pt-5">{error}</h4>}
 
-            <Col lg="12">
-              <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
-                {[...Array(pageCount).keys()].map((number) => (
-                  <span
-                    key={number}
-                    onClick={() => setPage(number)}
-                    className={page === number ? "active_page" : ""}
-                  >
-                    {number + 1}
-                  </span>
-                ))}
-              </div>
-            </Col>
-          </Row>
+          {!loading && !error && (
+            <Row>
+              {tours?.map((tour) => (
+                <Col lg="3" className="mb-4" key={tour._id}>
+                  {" "}
+                  <TourCard tour={tour}></TourCard>
+                </Col>
+              ))}
+
+              <Col lg="12">
+                <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
+                  {[...Array(pageCount).keys()].map((number) => (
+                    <span
+                      key={number}
+                      onClick={() => setPage(number)}
+                      className={page === number ? "active_page" : ""}
+                    >
+                      {number + 1}
+                    </span>
+                  ))}
+                </div>
+              </Col>
+            </Row>
+          )}
         </Container>
       </section>
       <Newsletter></Newsletter>
