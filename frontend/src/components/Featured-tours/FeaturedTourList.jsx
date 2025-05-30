bd000fc681032ed7f83abd67bd3d87ec96e19f5e
Trung Nguyễn Đức <23521678@ms.uit.edu.vn>
2025-05-30 17:49:22 +0700

    fetch login, register, logout

--- frontend/src/components/Featured-tours/FeaturedTourList.jsx
+++ frontend/src/components/Featured-tours/FeaturedTourList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import TourCard from "../../shared/TourCard";
-import tourData from "../../assets/data/tours";
+
 import { Col } from "reactstrap";
 
 import useFetch from "../../hooks/useFetch";
@@ -13,7 +13,6 @@ const FeaturedTourList = () => {
     error,
   } = useFetch(`${BASE_URL}/tours/search/getFeaturedTours`);
 
-  console.log(featuredTours);
   return (
     <>
       {loading && <h4>Loading..............</h4>}
@@ -21,7 +20,7 @@ const FeaturedTourList = () => {
       {!loading &&
         !error &&
         featuredTours?.map((tour) => (
-          <Col lg="3" className="mb-4" key={tour.id}>
+          <Col lg="3" className="mb-4" key={tour._id}>
             <TourCard tour={tour} />
           </Col>
         ))}
